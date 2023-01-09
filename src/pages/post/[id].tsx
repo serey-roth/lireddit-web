41444162c7f374003fc8dd032a215908992bcb84
Serey Roth <sereyratanakroth@gmail.com>
2023-01-08 23:32:18 -0800

    Add edit and delete buttons

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,27 +1,32 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/router';
 import React from 'react'
-import { useQuery } from 'urql';
+import { useMutation, useQuery } from 'urql';
+import EditDeletePostButtons from '../../components/EditDeletePostButtons';
 import Layout from '../../components/Layout';
-import { PostDocument, Post } from '../../gql/graphql';
+import { DeletePostDocument, MeDocument, Post, PostDocument, User } from '../../gql/graphql';
 import { createUrqlClient } from '../../util/createUrqlClient';
+import { useGetIntId } from '../../util/useGetIntId';
 
-interface PostProps {
-
-}
-
-const Post: React.FC<PostProps> = ({}) => {
+const Post: React.FC<{}> = ({}) => {
     const router = useRouter();
-    const intId = typeof router.query.id === 'string' ? parseInt(router.query.id) : -1;
+    const intId = useGetIntId();
     const [{ data, fetching, error }] = useQuery({ 
         query: PostDocument,
         pause: intId === -1,
         variables: {
-            id: intId,
+            id: intId
         }
-    })
+    });
+    const [{ data: meData }] = useQuery({ query: MeDocument });
+    const [, deletePost] = useMutation(DeletePostDocument);
 
+    const handleDeletePost = async (id: number) => {
+        await deletePost({ id });
+        router.push('/');
+    };
+    
     if (fetching) {
         return <Layout>
             <div>loading...</div>
@@ -34,7 +39,7 @@ const Post: React.FC<PostProps> = ({}) => {
 
     if (!data?.post) {
         return <Layout>
-            <div>loading...</div>
+            <div>cannot find post</div>
         </Layout>
     }
 
@@ -43,7 +48,13 @@ const Post: React.FC<PostProps> = ({}) => {
     return (
         <Layout>
             <Heading>{post.title}</Heading>
-            <Text mt={4}>{post.text}</Text>
+            <Text mt={4} mb={12}>{post.text}</Text>
+            <Box>
+                {(meData?.me as User)?.id === (data.post as Post)?.creatorId &&
+                <EditDeletePostButtons
+                    id={intId}
+                    onDelete={handleDeletePost} />}
+            </Box>
         </Layout>
     );
 }
